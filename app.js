4f7f35122796d175f7c844e82d4cc4b508a5d09c
Bikram <bikram.dangol19@gmail.com>
2019-03-17 12:18:05 +0545

    create server

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log("Hello world")
+
+const http = require("http")
+
+const server = http.createServer(function(request, response){
+response.end("Hello World")
+})
+
+server.listen(3000, function(){
+console.log("server is running")
+})
